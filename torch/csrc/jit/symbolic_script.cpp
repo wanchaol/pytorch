ff8fe1ded10d019543561d0d9e5bc104296701e2
Wanchao Liang <wanchaol@users.noreply.github.com>
2019-04-29 11:59:55 -0700

    temp disable layernorm ad

--- torch/csrc/jit/symbolic_script.cpp
+++ torch/csrc/jit/symbolic_script.cpp
@@ -1044,7 +1044,7 @@ const std::vector<std::string> functions = {
 
             return output, backward
 
-        def layer_norm(input : Tensor,
+        def layer_norm_disable(input : Tensor,
                        normalized_shape : List[int],
                        weight : Optional[Tensor],
                        bias : Optional[Tensor],
